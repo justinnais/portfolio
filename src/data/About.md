9c7047c0672652e4ec7e2bc745648a281480fe0c
justin naismith <justin.n@live.com.au>
2022-07-17 12:39:20 +1000

    Added about data

---
+++ src/data/About.md
@@ -0,0 +1,10 @@
+A final year student at RMIT with industry experience across the full stack 💾
+<br />
+
+Primary tech stack includes TypeScript, React and AWS. Always looking to learn and expand my skillset.
+<br />
+
+Passionate about building things to improve user and developer experience. Accessible web, infrastructure as code, internal tooling.
+<br />
+
+Former snowsports instructor ⛷
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,6 +4,7 @@
   import Skill from '$lib/components/Skill.svelte';
   import { skillColorMap } from '../data/skills';
   const map = new Map(Object.entries(skillColorMap));
+  import About from '../data/About.md';
 </script>
 
 <svelte:head>
@@ -13,20 +14,7 @@
 <Hero />
 <div class="flex flex-col gap-12">
   <Section title={'About'}>
-    <article class="flex flex-col gap-4 mb-8">
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda hic
-        ut molestias pariatur voluptas culpa velit enim at veniam ullam saepe
-        reiciendis mollitia dignissimos voluptatibus similique nobis, numquam
-        sed fugiat.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda hic
-        ut molestias pariatur voluptas culpa velit enim at veniam ullam saepe
-        reiciendis mollitia dignissimos voluptatibus similique nobis, numquam
-        sed fugiat.
-      </p>
-    </article>
+    <About />
   </Section>
   <Section title={'Skills'}
     >some kind of skill tree

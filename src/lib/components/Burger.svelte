dbd0377778bcdd4dc4b8354324b8a06f6dfc1a45
justin naismith <justin.n@live.com.au>
2022-07-18 19:31:11 +1000

    Added burger menu

---
+++ src/lib/components/Burger.svelte
@@ -0,0 +1,5 @@
+<div class="h-10 w-10 p-2 flex flex-col justify-around">
+  <div class="h-[3px] bg-gray-700 rounded" />
+  <div class="h-[3px] bg-gray-700 rounded" />
+  <div class="h-[3px] bg-gray-700 rounded" />
+</div>
--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,24 +1,52 @@
-<header class="flex justify-between items-center px-8 py-4 bg-blue-300">
-  <div class="flex flex-1 justify-center">
-    <a class="mr-auto text-3xl font-semibold" href="/">nais</a>
+<script>
+  import Burger from '$lib/components/Burger.svelte';
+  let menuOpen = false;
+
+  const handleClick = () => (menuOpen = !menuOpen);
+</script>
+
+<header class="px-8 py-4">
+  <div class="flex justify-between items-center">
+    <div class="flex flex-1 justify-center">
+      <a class="mr-auto text-3xl font-semibold" href="/">nais</a>
+    </div>
+
+    <div class="flex-1 justify-center hidden sm:flex">
+      <nav>
+        <ul class="flex gap-8 list-none">
+          <li>
+            <a href="#about">about</a>
+          </li>
+          <li>
+            <a href="/projects">projects</a>
+          </li>
+          <li>
+            <a href="#contact">contact</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <div class="flex flex-1 justify-center">
+      <button
+        on:click={handleClick}
+        class="ml-auto w-auto sm:hidden border rounded"><Burger /></button
+      >
+    </div>
   </div>
 
-  <div class="flex-1 justify-center hidden sm:flex">
+  {#if menuOpen}
     <nav>
-      <ul class="flex gap-8 list-none">
+      <ul class="flex flex-col gap-4 list-none items-end p-2">
         <li>
           <a href="#about">about</a>
         </li>
         <li>
-          <a href="#projects">projects</a>
+          <a href="/projects">projects</a>
         </li>
         <li>
           <a href="#contact">contact</a>
         </li>
       </ul>
     </nav>
-  </div>
-  <div class="flex flex-1 justify-center">
-    <button class="ml-auto w-auto sm:hidden">burger</button>
-  </div>
+  {/if}
 </header>

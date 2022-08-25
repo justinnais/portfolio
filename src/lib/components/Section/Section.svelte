ba15dbbdd0df204588eee8f009534d5371b13189
Justin Naismith <justin@nais.dev>
2022-08-25 23:57:06 +1000

    Intersection observer

--- src/lib/components/Section/Section.svelte
+++ src/lib/components/Section/Section.svelte
@@ -1,8 +1,23 @@
 <script>
   export let title;
+  import IntersectionObserver from 'svelte-intersection-observer';
+  import { fade } from 'svelte/transition';
+
+  let node;
 </script>
 
-<section class="md:mb-16" id={title.toLowerCase()}>
-  <h2 class="mb-8">{title}</h2>
-  <slot />
-</section>
+
+<IntersectionObserver once element={node} let:intersecting>
+  <div bind:this={node}>
+    {#if intersecting}
+      <section
+        class="md:mb-16"
+        id={title.toLowerCase()}
+        transition:fade={{ delay: 250 }}
+      >
+        <h2 class="mb-8">{title}</h2>
+        <slot />
+      </section>
+    {/if}
+  </div>
+</IntersectionObserver>

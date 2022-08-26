2e93271636daee0eb9fc6d93a1f13b9ffe7dce92
Justin Naismith <justin@nais.dev>
2022-08-26 11:09:30 +1000

    Logos added to buttons and footers

--- src/lib/components/Footer/Footer.svelte
+++ src/lib/components/Footer/Footer.svelte
@@ -1,3 +1,7 @@
-<footer class="flex justify-center w-full px-4 py-2">
-  <a href="https://github.com/justinnais">GitHub</a>
+<script>
+  import GitHub from '$lib/assets/icons/GitHub.svelte';
+</script>
+
+<footer class="flex justify-center w-full px-4 py-4">
+  <a href="https://github.com/justinnais" class="no-underline"><GitHub /></a>
 </footer>
--- src/routes/projects/_project.svelte
+++ src/routes/projects/_project.svelte
@@ -1,10 +1,17 @@
 <script>
   import { Button, Tag } from '$lib/components';
+  import GitHub from '$lib/assets/icons/GitHub.svelte';
+  import Link from '$lib/assets/icons/Link.svelte';
   export let name;
   export let date;
   export let tags;
   export let links;
   const linkMap = new Map(Object.entries(links || {}));
+
+  const icons = new Map([
+    ['github', GitHub],
+    ['external', Link],
+  ]);
 </script>
 
 <svelte:head>
@@ -26,7 +33,11 @@
   {#if links}
     <ul class="flex gap-2 flex-wrap">
       {#each [...linkMap] as [type, url]}
-        <li><Button href={url}>{type}</Button></li>
+        <li>
+          <Button icon={true} href={url} underline={false}
+            ><svelte:component this={icons.get(type)} /></Button
+          >
+        </li>
       {/each}
     </ul>
   {/if}

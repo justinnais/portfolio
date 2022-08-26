7c566103d7dde02738ff740a12c71008bee7202d
Justin Naismith <justin@nais.dev>
2022-08-26 18:55:11 +1000

    Updated portfolio link

--- src/lib/components/Footer/Footer.svelte
+++ src/lib/components/Footer/Footer.svelte
@@ -4,5 +4,5 @@
 </script>
 
 <footer class="flex justify-center w-full px-4 py-4">
-  <a href="https://github.com/justinnais" class="inline-flex gap-1 no-underline items-center"><span>Made with</span><Svelte size="20" /><span>on</span><GitHub size="20" /></a>
+  <a href="https://github.com/justinnais/portfolio" class="inline-flex gap-1 no-underline items-center"><span>Made with</span><Svelte size="20" /><span>on</span><GitHub size="20" /></a>
 </footer>

3346c102a1e23c5ac396417f87ad3f0c392c93c4
Justin Naismith <justin@nais.dev>
2022-08-26 11:45:36 +1000

    svelte icon

--- src/lib/components/Footer/Footer.svelte
+++ src/lib/components/Footer/Footer.svelte
@@ -1,7 +1,8 @@
 <script>
   import GitHub from '$lib/assets/icons/GitHub.svelte';
+  import Svelte from '$lib/assets/icons/Svelte.svelte';
 </script>
 
 <footer class="flex justify-center w-full px-4 py-4">
-  <a href="https://github.com/justinnais" class="no-underline"><GitHub /></a>
+  <a href="https://github.com/justinnais" class="inline-flex gap-1 no-underline items-center"><span>Made with</span><Svelte size="20" /><span>on</span><GitHub size="20" /></a>
 </footer>

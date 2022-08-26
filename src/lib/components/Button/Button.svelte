d39b7c34f096a5945c45c379a9fd32c2bc0fb78c
Justin Naismith <justin@nais.dev>
2022-08-26 11:09:51 +1000

    Updated styling to prevent underlines on logos

--- src/lib/components/Button/Button.svelte
+++ src/lib/components/Button/Button.svelte
@@ -1,17 +1,27 @@
 <script>
   export let href;
-
-  const classList =
-    'bg-neutral-300 dark:bg-neutral-800 font-mono px-2 py-1 shadow hover:shadow-lg';
+  export let icon;
+  export let underline;
 </script>
 
 <!-- TODO don't like this, work out how to use as property properly -->
 {#if href}
-  <a class={classList} {href}>
+  <a class:icon class:no-underline={underline === false} {href}>
     <slot />
   </a>
 {:else}
-  <button class={classList}>
+  <button class:icon>
     <slot>Button</slot>
   </button>
 {/if}
+
+<style>
+  a,
+  button {
+    @apply bg-neutral-300 dark:bg-neutral-800 font-mono px-2 py-1 shadow hover:shadow-lg;
+  }
+
+  .icon {
+    @apply bg-transparent dark:bg-transparent;
+  }
+</style>
